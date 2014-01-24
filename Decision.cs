d43f1b42dd0f1075768f5e8ca75c90137461711f
Roberto Liffredo <rliffredo@gmail.com>
2014-01-24 19:41:14 +0100

    Added decision maker

---
+++ Decision.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships
+{
+    class Decision
+    {
+        Tuple<int, int> CellToAttack()
+        {
+            var r = new Random();
+            return Tuple.Create(r.Next(10), r.Next(10));
+        }
+    }
+
+    enum CellStates
+    {
+        Water,
+        Hit,
+        Sunk,
+        Nil
+    }
+    class BoardState
+    {
+    }
+}
